376b47186ad8a635e6d24a5be2c1ebd9553b85f5
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2023-12-08 09:41:07 +0100

    Add support for SSR (as client-side library)

---
+++ tests/module.spec.js
@@ -0,0 +1,17 @@
+import Pikaday from '../';
+
+describe('Pikaday', () => {
+  it('should expose as a CommonJS module', () => {
+      expect(typeof Pikaday).toBe('function');
+  });
+
+  it('should NOT leak to the global object', () => {
+      expect(window.Pikaday).toBeUndefined();
+  });
+
+  it('should be possible to create a new instance', () => {
+      expect(() => {
+        new Pikaday();
+      }).not.toThrow();
+  });
+});
